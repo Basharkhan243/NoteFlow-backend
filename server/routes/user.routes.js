7f0bec9c78da1f3ee267da0ccaaddcbcf62ae7bf
Bashar Ahmad Khan <150275763+Basharkhan243@users.noreply.github.com>
2025-10-25 19:13:57 +0530

    Add files via upload

---
+++ server/routes/user.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  RegisterUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.route("/signup").post(RegisterUser);
+router.route("/login").post(loginUser);
+// secure route
+router.route("/logout").post(verifyJWT, logoutUser);
+export default router;
